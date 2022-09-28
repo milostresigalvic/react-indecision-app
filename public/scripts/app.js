82adc37f41e06a50fdf63ebd1e714a5c50401cb8
Milos <milos.tresiglavic@gmail.com>
2022-09-28 14:24:58 +0100

    some edits

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,79 +3,47 @@
 // CMD run project 
 // babel src/app.js --out-file=public/scripts/app.js --presets="env,react" --watch
 
+// other cmd run: 
+// C:\React\react-course-projects\react-indecision-app
+// live-server public
+
 var app = {
     title: "This is app title.",
     subtitle: "This is app subtitle.",
-    options: ['One', 'Two']
-
-    // JSX - JavaScript XML
-};var template = React.createElement(
-    "div",
-    null,
-    React.createElement(
-        "h1",
-        null,
-        app.title
-    ),
-    app.subtitle && React.createElement(
-        "p",
-        null,
-        app.subtitle
-    ),
-    React.createElement(
-        "p",
-        null,
-        app.options.length > 0 ? "here are your options" : "No options"
-    ),
-    React.createElement(
-        "ol",
-        null,
-        React.createElement(
-            "li",
-            null,
-            "Item one"
-        ),
-        React.createElement(
-            "li",
-            null,
-            "Item two"
-        )
-    )
-);
-var user = {
-    name: "Mike",
-    age: 12,
-    location: "Philadelphia"
+    options: [11, 21, 31]
 };
-function getLocation(location) {
-    if (location) {
-        return React.createElement(
-            "p",
-            null,
-            "Location: ",
-            location
-        );
-    }
-}
-var template2 = React.createElement(
-    "div",
-    null,
-    React.createElement(
-        "h1",
-        null,
-        user.name ? user.name : "Anonymous"
-    ),
-    user.age && user.age >= 18 && React.createElement(
-        "p",
-        null,
-        "Age:",
-        user.age,
-        " "
-    ),
-    getLocation(user.location)
-);
+
+var Visibility = true;
+
+var onToogle = function onToogle(e) {
+    Visibility = !Visibility;
+    renderContent();
+};
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+var renderContent = function renderContent() {
+    // JSX - JavaScript XML
+    var template = React.createElement(
+        "div",
+        null,
+        React.createElement(
+            "h1",
+            null,
+            app.title
+        ),
+        React.createElement(
+            "button",
+            { onClick: onToogle },
+            Visibility == true ? "Hide details" : "Show details"
+        ),
+        React.createElement(
+            "p",
+            null,
+            Visibility && "Some details here..."
+        )
+    );
 
-console.log();
+    ReactDOM.render(template, appRoot);
+};
+
+renderContent();
